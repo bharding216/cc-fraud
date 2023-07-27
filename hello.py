5417b732dbefb64a09db5b4f82fba7d0a54e634b
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2023-07-27 16:29:26 -0500

    Update to hello.py routes

--- hello.py
+++ hello.py
@@ -9,17 +9,20 @@ load_dotenv()
 
 api_url = os.getenv('API_URL')
 
+# Flask + SPA Docs: https://flask.palletsprojects.com/en/2.3.x/patterns/singlepageapplications/
 
-@app.route("/", defaults={"path": ""})
-def client():
-    return send_from_directory('client/public', 'index.html')
+# @app.route("/", defaults={"path": ""})
+# def client():
+#     return send_from_directory('client/public', 'index.html')
 
-@app.route("/<path:path>")
-def serve_static(path):
+# @app.route("/<path:path>")
+# def serve_static(path):
+#     return send_from_directory('client/public', path)
+
+@app.route('/', defaults={'path': ''}) # Capture the root URL endpoint
+@app.route('/<path:path>') # Captures any URL endpoint
+def catch_all(path):
     return send_from_directory('client/public', path)
-
-
-
 
 
 # API endpoint for Svelte to communicate with.
